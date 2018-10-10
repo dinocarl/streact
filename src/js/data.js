99da96dccfc2ce947e28d13de9ca9c2125f3dc9b
Carl Albrecht-Buehler <carl@dinocarl.com>
2018-10-10 13:57:35 -0500

    Fixing eslint errors in `data.js`

--- src/js/data.js
+++ src/js/data.js
@@ -1,20 +1,20 @@
 // Default Data
 
 export default {
-  title: 'Matching Game with stReact',
+  title: `Matching Game with stReact`,
   cards: [
-    {val: '💀', dir: 'd'}, {val: '🦄', dir: 'd'}, {val: '💖', dir: 'd'}, 
-    {val: '🦊', dir: 'u'}, {val: '😎', dir: 'd'}, {val: '😄', dir: 'd'}, 
-    {val: '🦇', dir: 'd'}, {val: '😱', dir: 'd'}, {val: '😬', dir: 'd'}, 
-    {val: '🍧', dir: 'd'}, {val: '👻', dir: 'd'}, {val: '🐨', dir: 'd'}, 
-    {val: '😬', dir: 'd'}, {val: '😎', dir: 'd'}, {val: '🦊', dir: 'u'}, 
-    {val: '👻', dir: 'd'}, {val: '💀', dir: 'd'}, {val: '🍧', dir: 'd'}, 
-    {val: '🐨', dir: 'd'}, {val: '🦄', dir: 'd'}, {val: '💖', dir: 'd'}, 
-    {val: '😄', dir: 'd'}, {val: '😱', dir: 'd'}, {val: '🦇', dir: 'd'},
+    { val: `💀`, dir: `d` }, { val: `🦄`, dir: `d` }, { val: `💖`, dir: `d` },
+    { val: `🦊`, dir: `u` }, { val: `😎`, dir: `d` }, { val: `😄`, dir: `d` },
+    { val: `🦇`, dir: `d` }, { val: `😱`, dir: `d` }, { val: `😬`, dir: `d` },
+    { val: `🍧`, dir: `d` }, { val: `👻`, dir: `d` }, { val: `🐨`, dir: `d` },
+    { val: `😬`, dir: `d` }, { val: `😎`, dir: `d` }, { val: `🦊`, dir: `u` },
+    { val: `👻`, dir: `d` }, { val: `💀`, dir: `d` }, { val: `🍧`, dir: `d` },
+    { val: `🐨`, dir: `d` }, { val: `🦄`, dir: `d` }, { val: `💖`, dir: `d` },
+    { val: `😄`, dir: `d` }, { val: `😱`, dir: `d` }, { val: `🦇`, dir: `d` },
   ],
   matched: [[3, 14]],
   attempt: [],
-  mode: 'play',
-  correctLabel: 'Matched Pairs:',
+  mode: `play`,
+  correctLabel: `Matched Pairs:`,
   numCorrect: 1
 };
